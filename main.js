254ddba65786addbb9be3ee56628a8ac6eac4ae5
Dewan <iit2015097@iiita.ac.in>
2016-11-11 18:38:18 +0530

    electron window connected with localhost

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ const {app, BrowserWindow} = electron
 
 app.on('ready', () => {
 	let win = new BrowserWindow({width:800, height:600})
-	win.loadURL(`file://${__dirname}/index.html`)
+	win.loadURL(`http://localhost:8080`)
 })
