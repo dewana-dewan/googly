5465785115a35b499000696d1a1a45700fb80429
Dewan <iit2015097@iiita.ac.in>
2016-11-13 12:49:33 +0530

    basic user signup added

--- main.py
+++ main.py
@@ -1,25 +1,96 @@
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import webapp2
+import jinja2
+import os
+import re
+import hashlib
+import hmac
 
+from google.appengine.ext import db
 
-class MainPage(webapp2.RequestHandler):
+#base template start
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+	
+class BaseHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        # params['user'] = self.user
+        return render_str(template, **params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+#base template end
+
+class User(db.Model):
+	name = db.StringProperty(required=True)
+	pw = db.StringProperty(required=True)
+	email = db.StringProperty()
+
+#functions for basic sign-up
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
+class SignUp(BaseHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hey, World!')
+        self.render('signup-form.html')
+
+    def post(self):
+    	uname = self.request.get('username')
+    	password = self.request.get('password')
+    	verify = self.request.get('verify')
+    	email = self.request.get('email')
+    	have_error = False
+    	params = dict(username = uname,
+                      email = email)
+    	if not valid_username(uname):
+            params['error_username'] = "That's not a valid username."
+            have_error = True
+
+        if not valid_password(password):
+            params['error_password'] = "That wasn't a valid password."
+            have_error = True
+        elif password != verify:
+            params['error_verify'] = "Your passwords didn't match."
+            have_error = True
+
+        if not valid_email(email):
+            params['error_email'] = "That's not a valid email."
+            have_error = True
+
+        if have_error:
+            self.render('signup-form.html', **params)
+        else:
+            self.redirect('/welcome?username=' + uname)
+
+class Welcome(BaseHandler):
+    def get(self):
+        username = self.request.get('username')
+        if valid_username(username):
+            self.render('welcome.html', username = username)
+        else:
+            self.redirect('/sign_up')
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/sign_up', SignUp),
+    ('/welcome', Welcome)
 ], debug=True)
